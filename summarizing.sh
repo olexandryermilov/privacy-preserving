02f9fa3f6d39e00da134b3281ab8c92e23ad4e84
oleksandryermilov <olexandr.yermilov@gmail.com>
2022-02-14 09:45:00 +0200

    improvements

--- summarizing.sh
+++ summarizing.sh
@@ -10,8 +10,8 @@ pip install sentencepiece
 pip install gdown
 gdown --id 0BwmD_VLjROrfTTljRDVZMFJnVWM
 gdown --id 0BwmD_VLjROrfM1BxdkxVaTY2bWs
-tar zxvf cnn_stories.tgz
-tar zxvf dailymail_stories.tgz
+#tar zxvf cnn_stories.tgz
+#tar zxvf dailymail_stories.tgz
 git clone https://github.com/artmatsak/cnn-dailymail.git
 python3 cnn-dailymail/make_datafiles.py cnn/stories dailymail/stories
 #preprocess data
@@ -21,8 +21,8 @@ git clone https://github.com/THUDM/GLM
 cd GLM
 #change ds_finetune in case I have my own dataset path
 yes | cp -rf ../privacy-preserving/ds_finetune_seq2seq.sh ./scripts/
-pip install pytorch
-pip install requirements.txt
+pip3 install pytorch
+pip3 install -r requirements.txt
 #run training
 bash scripts/ds_finetune_seq2seq.sh \
    config_tasks/model_blocklm_10B.sh \
