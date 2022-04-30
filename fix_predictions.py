553d2fc9e8bd47e8101bfe188fc259ae706cdef2
oleksandry <oleksandry@wix.com>
2022-04-30 17:27:22 +0200

    smol fix of fix_predictions.py

--- fix_predictions.py
+++ fix_predictions.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from tqdm import tqdm
 
 def main():
     jsonpath = sys.argv[1]
@@ -8,9 +9,7 @@ def main():
         data = json.load(json_file)
         with open(predictions_path, "r") as predictions_file:
             corpus = predictions_file.read()
-            for k in sorted(data, key=lambda k: len(data[k]), reverse=True):
-                if data[k] in corpus or data[k] =="PERSON_11":
-                    print(f"{data[k]} {k}")
+            for k in tqdm(sorted(data, key=lambda k: len(data[k]), reverse=True)):
                 corpus = corpus.replace(data[k], k)
             with open("fixed_predictions.txt", "w") as new_predictions:
                 new_predictions.write(corpus)
