b183a1eea6e96806b9d1697e92d785b4a5daa59b
oleksandry <oleksandry@wix.com>
2022-05-04 03:05:38 +0200

    statistics

--- statistics.py
+++ statistics.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from tqdm import tqdm
 
 def main():
     path_sum = sys.argv[1]
@@ -7,13 +8,13 @@ def main():
     with open(path_sum, "r") as f:
         lines = f.readlines()
         res = 0
-        for line in lines:
+        for line in tqdm(lines):
             res += len(re.findall(r'\w+', line))
         print(f"summ: {res/len(lines)}")
     with open(path_tr, "r") as f:
         lines = f.readlines()
         res = 0
-        for line in lines:
+        for line in tqdm(lines):
             res += len(re.findall(r'\w+', line))
         print(f"tr: {res/len(lines)}")
 main()
